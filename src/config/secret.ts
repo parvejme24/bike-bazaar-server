38e2a16a2d01041bd7ab6b4da718635cbcec49e1
Md Parvej <mdparvejme24@gmail.com>
2025-02-22 01:19:28 +0600

    added s.txt

--- src/config/secret.ts
+++
@@ -1,5 +0,0 @@
-export const secretConfig = {
-  mongoDbUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nu02d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-  REFRESH_TOKEN: process.env.REFRESH_TOKEN,
-};
